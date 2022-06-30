38427aac76f7fa6f14dc30c10ce363738ae28341
oddishhorde <91993779+oddishhorde@users.noreply.github.com>
2022-06-30 08:58:47 -0500

    added TicketModel

--- ExpenseReimbursementSystem/Models/UserModel.cs
+++ ExpenseReimbursementSystem/Models/UserModel.cs
@@ -1,7 +1,21 @@
 ﻿namespace Models;
 public class UserModel
 {
-    public UserModel(){}
+    public UserModel()
+    {
+        UserId = 0;
+        Username = " ";
+        Password = " ";
+        Role = " ";
+    }
+
+    public UserModel(int Id, string name,string pass, string userRole)
+    {
+        UserId = Id;
+        Username = name;
+        Password = pass;
+        Role = userRole;
+    }
 
     public int UserId{get;set;}
     public string Username{get;set;}
